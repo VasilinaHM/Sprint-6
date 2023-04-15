0020caa829c45f35c7fb4d6403e029ab58fccd21
Vasilina Kirillova <vasilinahm@yandex.ru>
2023-04-15 03:38:18 +0300

    Correction test class Lion

--- src/test/java/LionParameterizedTest.java
+++ src/test/java/LionParameterizedTest.java
@@ -7,32 +7,31 @@ import org.mockito.Mockito;
 import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class LionParameterizedTest {
-   private Feline feline = Mockito.mock(Feline.class);
-   private String sex;
-   private Boolean hasMane = true;
+    private Feline feline = Mockito.mock(Feline.class);
+    private String sex;
+    private Boolean hasMane = true;
 
-    public LionParameterizedTest(String sex , Boolean hasMane)
-    {
+    public LionParameterizedTest(String sex, Boolean hasMane) {
         this.sex = sex;
         this.hasMane = hasMane;
     }
+
     @Parameterized.Parameters
     public static Object[][] data() {
         return new Object[][]{
                 {"Самка", false},
                 {"Самец", true},
-                {"оно" , null}
+                {"оно", null}
         };
     }
+
     @Test
-    public void testDoesHaveMane()  {
+    public void testDoesHaveMane() {
         try {
             Lion lion = new Lion(sex, feline);
             assertEquals(hasMane, lion.doesHaveMane());
-        }
-        catch (Exception e)
-        {
-            e.getMessage();
+        } catch (Exception ex) {
+            assertEquals("Используйте допустимые значения пола животного - самец или самка", ex.getMessage());
         }
     }
 }
